487571a881165fdff212790ce25830645e82ece5
Eliana <eliana04112001@gmail.com>
2024-10-20 15:00:39 -0500

    bugfix: Renaming functions, to avoid collisions

--- BusinessLogic/Controllers/PersonController.py
+++ BusinessLogic/Controllers/PersonController.py
@@ -3,7 +3,7 @@ from Models.person import Person
 from bson import ObjectId
 
 
-async def createPerson(personData: Person):
+async def createPersonController(personData: Person):
     newPerson = personData.dict()
     result = await personCollection.insert_one(newPerson)
     return str(result.inserted_id)
