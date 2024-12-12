33649599fa6e6179b5d66f73d44a943044003170
JulianaChM <julianchamorromarin@gmail.com>
2024-12-12 00:59:29 -0500

    refactor:delete code smells

--- BusinessLogic/Controllers/PersonController.py
+++ BusinessLogic/Controllers/PersonController.py
@@ -5,6 +5,7 @@ from Config.DatabaseConnection import personCollection, registerCollection
 from Models.person import Person
 from datetime import datetime, timedelta
 import bcrypt
+from typing import Any
 
 # Function to encript the password
 
@@ -120,6 +121,61 @@ async def deletePersonController(personId: str):
 
     return {"message": "Persona eliminada exitosamente con los registros antiguos"}
 
+""" async def deletePersonController(personId: str):
+    # Buscar la persona
+    person = await personCollection.find_one({"_id": ObjectId(personId)})
+    if not person:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+
+    # Validar código de persona
+    personCode = person.get("code")
+    if not personCode:
+        raise HTTPException(
+            status_code=400, detail="Código de persona no encontrado"
+        )
+
+    # Verificar registros relacionados
+    await validate_related_registers(personCode)
+
+    # Eliminar registros antiguos
+    await delete_old_registers(personCode)
+
+    # Eliminar la persona
+    result = await personCollection.delete_one({"_id": ObjectId(personId)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+
+    return {"message": "Persona eliminada exitosamente con los registros antiguos"}
+
+
+async def validate_related_registers(personCode: Any):
+    relatedRegisters = await registerCollection.find({"personCode": personCode}).to_list(length=None)
+
+    for register in relatedRegisters:
+        dateTimeExit = register.get("dateTimeExit")
+
+        if dateTimeExit is None:
+            raise HTTPException(
+                status_code=400,
+                detail="No se puede eliminar el registro si no se ha registrado la salida",
+            )
+
+        time_diff = datetime.now() - dateTimeExit
+        if time_diff <= timedelta(days=15):
+            raise HTTPException(
+                status_code=400,
+                detail="No se puede eliminar el registro antes de 15 días desde la salida",
+            )
+
+
+async def delete_old_registers(personCode: Any):
+    await registerCollection.delete_many(
+        {
+            "personCode": personCode,
+            "dateTimeExit": {"$lte": datetime.now() - timedelta(days=15)},
+        }
+    )
+ """
 
 # Function to get a person by code
 async def getPersonByCode(personCode: int):
